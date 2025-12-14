44343732fb7aaa169e08baa6ed1f6ca101e13a1a
trinhthihue2408-design <trinhthihue2408@gmail.com>
2025-12-14 10:28:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import tempfile
 import os
 import io
 import re
-from docx import Python.docx
+from docx import Document
 from docx.shared import Pt, RGBColor, Inches, Cm
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 
